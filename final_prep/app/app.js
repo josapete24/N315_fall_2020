dd2eed151ba11927390915cf333b8e8d27e793e1
josapete24 <jowens1678@yahoo.com>
2020-11-02 08:53:23 -0500

    More practice for the final

--- final_prep/app/app.js
+++ final_prep/app/app.js
@@ -1,13 +1,61 @@
-function init(){
-    $(".navicon").click(function(e){
-        $("nav").addClass("navMobileView");
-        if($(".links").is(":hidden")){
-            $(".links").show();
-        }else if($(".links").is(":visible")) {
-            $(".links").hide();
-        } ;
-        console.log("true");
-    })
+var obj = {
+  Students: [
+    {
+      firstName: "Joshua",
+      lastName: "Peterson",
+      classesAndGrades: [
+        {
+          className: "n315",
+          grade: "B",
+        },
+        {
+          className: "n215",
+          grade: "A",
+        },
+      ],
+    },
+    {
+      firstName: "John",
+      lastName: "Doe",
+    },
+  ],
+};
+
+function init() {
+  //get data button
+
+  // another form to put function is =>
+
+  //This is a for loop and can be used with pure javascript
+  $(".getData").click((e) => {
+    //   for(let i=0; i < obj.Students.length; i++){
+    //      console.log("obj " + i + " " , obj.Students[i]);
+    //  };
+
+    //This graes the first name of the student
+    $.each(obj.Students, (idx, student) => {
+      console.log(student.firstName);
+      $(".content").append(student.firstName + "<br />");
+
+      //This grabes the students grades
+      $.each(student.classesAndGrades, (idx, classesAndGrade) => {
+        console.log(classesAndGrade.className + " " + classesAndGrade.grade);
+        $(".content").append(classesAndGrade.className + " "+ classesAndGrade.grade  + "<br />" )
+    });
+    });
+  });
+
+  //NavIcon
+  $(".navicon").click(function (e) {
+    $("nav").toggleClass("navMobileView");
+    console.log($(".links").css("display"));
+    if ($(".links").css("display") == "none") {
+      $(".links").css("display", "flex");
+    } else {
+      $(".links").css("display", "none");
+    }
+    //console.log("click");
+  });
 }
 
 $(document).ready(init());
